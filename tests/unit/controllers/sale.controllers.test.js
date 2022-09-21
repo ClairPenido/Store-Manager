ed659880878d3a0a252d4ba571efebc03259d529
Clair Penido <clairpenido@gmail.com>
2022-09-20 22:25:24 -0300

    mais testes

--- tests/unit/controllers/sale.controllers.test.js
+++ tests/unit/controllers/sale.controllers.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const { allSalesMock } = require('../mocks/sale.mock');
+const { allSalesMock, saleIdMock } = require('../mocks/sale.mock');
 
 const saleController = require('../../../src/controllers/sales.controller');
 const saleService = require('../../../src/services/sales.service');
@@ -22,5 +22,31 @@ describe('Teste da Camada Controller Sales', () => {
     await saleController.getAllSales(req, res);
     expect(res.status.calledWith(200));
     expect(res.json.calledWith(allSalesMock));
+  });
+
+  it('Listar todos os Sales, se possuir sale com esse ID', async function () {
+    const req = {params: {id: 1}};
+    const res = {};
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(saleService, 'returnSaleByID')
+      .resolves(saleIdMock)
+    await saleController.getSalesById(req, res);
+    expect(res.status.calledWith(200));
+    expect(res.json.calledWith(saleIdMock));
+  });
+
+  it('Não listar um sale, se não existir sale com o', async function () {
+    const req = { params: { id: 10 } };
+    const res = {};
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(saleService, 'returnSaleByID')
+      .resolves([])
+    await saleController.getSalesById(req, res);
+    expect(res.status.calledWith(404));
+    expect(res.json.calledWith({ message: 'Sale not found' }));
   });
 })
--- tests/unit/mocks/sale.mock.js
+++ tests/unit/mocks/sale.mock.js
@@ -15,6 +15,21 @@ const allSalesMock =
 
   ];
 
+const saleIdMock = {
+  "id": 3,
+  "itemsSold": [
+    {
+      "productId": 1,
+      "quantity": 1
+    },
+    {
+      "productId": 2,
+      "quantity": 5
+    }
+  ]
+};
+
 module.exports = {
   allSalesMock,
+  saleIdMock,
 };
