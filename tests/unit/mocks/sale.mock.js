11f92cb442475d9f02b634e229871f09446c4ab9
Clair Penido <clairpenido@gmail.com>
2022-09-20 22:38:49 -0300

    teste da camada service

--- tests/unit/mocks/sale.mock.js
+++ tests/unit/mocks/sale.mock.js
@@ -15,19 +15,18 @@ const allSalesMock =
 
   ];
 
-const saleIdMock = {
-  "id": 3,
-  "itemsSold": [
-    {
-      "productId": 1,
-      "quantity": 1
-    },
-    {
-      "productId": 2,
-      "quantity": 5
-    }
-  ]
-};
+const saleIdMock = [
+  {
+    "date": "2021-09-09T04:54:29.000Z",
+    "productId": 1,
+    "quantity": 2
+  },
+  {
+    "date": "2021-09-09T04:54:54.000Z",
+    "productId": 2,
+    "quantity": 2
+  }
+];
 
 module.exports = {
   allSalesMock,
--- tests/unit/services/sale.services.test.js
+++ tests/unit/services/sale.services.test.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 
 const saleModel = require('../../../src/models/sales.model');
 const saleService = require('../../../src/services/sales.service');
-const { allSalesMock } = require('../mocks/sale.mock');
+const { allSalesMock, saleIdMock } = require('../mocks/sale.mock');
 
 describe('Teste da Camada Service', () => {
   afterEach(function () {
@@ -15,6 +15,14 @@ describe('Teste da Camada Service', () => {
     sinon.stub(saleModel, 'getAllSales').resolves(allSalesMock);
     const result = await saleService.returnAllSales();
     expect(result).to.deep.equal(allSalesMock);
+
+  });
+
+  it('Listar a Sale, se possuir sale com esse ID', async function () {
+
+    sinon.stub(saleModel, 'getSaleById').resolves(saleIdMock);
+    const result = await saleService.returnSaleByID(1);
+    expect(result).to.deep.equal(saleIdMock);
 
   });
 });
